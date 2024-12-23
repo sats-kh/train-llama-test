a66c7c0ffbb290029cc219ccc83ff129a2c9a9f5
Kwanhoon <kwanhoon@Kwanhoons-Mac-mini.local>
2024-12-23 17:57:38 +0900

    modifed path and chmod

--- run_master.sh
+++ run_master.sh
@@ -12,12 +12,12 @@ NUM_GPUS=1
 for ((GPU_ID=0; GPU_ID<$NUM_GPUS; GPU_ID++)); do
     echo "Launching on MASTER NODE GPU $GPU_ID"
 
-    source /home/kh/train-llama-test/llama-venv/bin/activate
+    source /home/kh/llama/train-llama-test/llama-venv/bin/activate
     nohup torchrun \
         --nproc_per_node=1 \
         --nnodes=5 \
         --node_rank=0 \
         --master_addr=$MASTER_ADDR \
         --master_port=$MASTER_PORT \
-        /home/kh/gpuc/train-llama-test/train.py --local_rank=$GPU_ID &
+        /home/kh/llama/train-llama-test/train.py --local_rank=$GPU_ID &
 done
